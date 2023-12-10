e4c18e521ae91740775f64f04bc81f1e00de6a0e
Jack <schoenjack98@gmail.com>
2023-12-10 13:15:10 -0500

    "radical" changes

--- config.py
+++ config.py
@@ -9,8 +9,8 @@ RANDOM_SEED = 42
 # These control how quickly the generator and discriminator learn. Too high, and they may overshoot optimal solutions; too low, and they may get stuck or learn very slowly.
 # If the discriminator learns too fast, it may overfit to the current generator's output and not provide useful gradients. If the generator's learning rate is too low in comparison, it may not catch up, leading to poor image quality.
 # These are the number of units in the dense layers of your generator and discriminator models. Increasing these can give the network more capacity to learn complex patterns, but too much complexity can lead to overfitting or longer training times.
-GEN_COMPLEXITY = 510
-DISC_COMPLEXITY = 77 # lower rate for the discriminator helps generator
+GEN_COMPLEXITY = 512
+DISC_COMPLEXITY = 64 # lower rate for the discriminator helps generator
 
 # --- Learning Rate & Scheduling ---
 # Learning rates for generator and discriminator
@@ -24,7 +24,7 @@ LR_DECAY_EPOCHS = None  # e.g., 100
 
 # --- Training Settings ---
 # Larger batches provide more stable gradients but may require more memory and computational power, while smaller batches can sometimes encourage diversity in the generated images and can lead to faster convergence but may also introduce more noise into the training process.
-BATCH_SIZE = 160
+BATCH_SIZE = 165
 # Total number of epochs for training
 EPOCHS = 5000
 
@@ -40,7 +40,7 @@ DROPOUT_RATE = 0.25  # e.g., 0.3
 # Noise values for training stability
 # The noise added to the labels helps to prevent the discriminator from becoming too confident. However, too much noise can destabilize training.
 FAKE_NOISE_VAL = 0.15
-REAL_NOISE_VAL = 0.25
+REAL_NOISE_VAL = 0.5
 # Discriminator's confidence threshold
 # lowering disc_confidence can help the generator learn better
 DISC_CONFIDENCE = 0.5
@@ -48,13 +48,13 @@ DISC_CONFIDENCE = 0.5
 # --- Validation & Monitoring ---
 # Interval for saving model checkpoints
 CHECKPOINT_INTERVAL = 10
-CHECKPOINT_EXT = ".keras"
 
 # --- File Paths ---
 # Paths for saving weights, logs, etc.
 GEN_WEIGHTS_PATH = "./gen"
 DISC_WEIGHTS_PATH = "./disc"
 LOG_DIR = "logs/"
+CHECKPOINT_EXT = ".keras"
 GEN_MODEL_PATH = "./gen_model_epoch_"
 DISC_MODEL_PATH = "./disc_model_epoch_"
 
