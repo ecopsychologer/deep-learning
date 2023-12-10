7c8deacb1e7b95d0a212fd4001db876618f427b6
Jack <schoenjack98@gmail.com>
2023-12-09 23:00:48 -0500

    update

--- saveNload.py
+++ saveNload.py
@@ -14,10 +14,10 @@ train_images = load_data()
 def generate_and_save_images(model, epoch, test_input, writer):
     predictions = model(test_input, training=False)
 
-    fig = plt.figure(figsize=(2, 8))
+    fig = plt.figure(figsize=(5, 5))
 
     for i in range(predictions.shape[0]):
-        plt.subplot(2, 8, i+1)
+        plt.subplot(5, 5, i+1)
         plt.imshow(predictions[i, :, :] * 127.5 + 127.5, cmap='gray')
         plt.axis('off')
 
