47e51dda860d49213bef8c3602b518202ffc33c3
Jack <schoenjack98@gmail.com>
2023-12-11 10:40:26 -0500

    redo

--- pics_to_gif.py
+++ pics_to_gif.py
@@ -1,39 +1,36 @@
 from PIL import Image
 import os, glob, re
 
-def find_latest_img(img_folder="./logs/", img_name="generated_plot_e", img_ext=".png"):
-    full_path = img_folder + img_name + "*" + img_ext
-    img_path = img_folder + img_name
+def find_latest_imgs(img_folder="./logs/", img_name="generated_plot_e", img_ext=".png"):
+    full_path = os.path.join(img_folder, img_name + "*" + img_ext)
     img_files = glob.glob(full_path)
-    imgs = [int(re.search(fr'{img_path}(\d+){img_ext}', file).group(1)) for file in img_files]
-    return max(imgs) if imgs else None
+    
+    latest_images = {}
+    for file in img_files:
+        match = re.search(fr'{img_name}(\d+)-(\d+){img_ext}', file)
+        if match:
+            img_num = int(match.group(1))
+            sub_img_num = int(match.group(2))
+            latest_images[img_num] = max(latest_images.get(img_num, 0), sub_img_num)
+    return latest_images
 
-def create_gif(frame_duration_ms=250, image_folder="./logs/", output_path="./results/epochs_0_to_", output_ext=".gif"):
-    # Find latest image
-    latest_img_number = find_latest_img(image_folder)
-    # Check
-    if latest_img_number is None:
+def create_gif(frame_duration_ms=250, image_folder="./logs/", output_path="./results/epochs_", output_ext=".gif"):
+    latest_images = find_latest_imgs(image_folder, "generated_plot_e", ".png")
+
+    if not latest_images:
         print("No images found in the specified folder.")
         return
-    
-    # Generate file paths for all available images up to the latest one
-    filenames = [f"generated_plot_e{str(i).zfill(3)}.png" for i in range(1, latest_img_number + 1)]
 
-    # Load images
-    images = []
-    for filename in filenames:
-        file_path = os.path.join(image_folder, filename)
-        if os.path.exists(file_path):
-            images.append(Image.open(file_path))
+    for img_num, max_sub_num in latest_images.items():
+        filenames = [f"generated_plot_e{str(img_num).zfill(3)}-{i}.png" for i in range(1, max_sub_num + 1)]
+        images = [Image.open(os.path.join(image_folder, filename)) for filename in filenames if os.path.exists(os.path.join(image_folder, filename))]
 
-    name = output_path + str(latest_img_number) + output_ext
-    # Ensure there are images to create a gif
-    if images:
-        # Save the images as a gif
-        images[0].save(name, save_all=True, append_images=images[1:], duration=frame_duration_ms, loop=1)
-        print(f"GIF successfully created from images through epoch {latest_img_number} and saved to {output_path}")
-    else:
-        print("No images found to create a gif.")
+        if images:
+            gif_name = f"{output_path}{str(img_num).zfill(3)}{output_ext}"
+            images[0].save(gif_name, save_all=True, append_images=images[1:], duration=frame_duration_ms, loop=1)
+            print(f"GIF successfully created for epoch {img_num} and saved to {gif_name}")
+        else:
+            print(f"No images found for epoch {img_num} to create a gif.")
 
 # Usage
 create_gif()  # frame_duration is in milliseconds
