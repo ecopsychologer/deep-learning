eedac831a0ddd998398823d7259eb13b0a9ebe7d
Jack <schoenjack98@gmail.com>
2023-12-13 13:17:32 -0500

    Update pics_to_gif.py

--- pics_to_gif.py
+++ pics_to_gif.py
@@ -1,5 +1,5 @@
 from PIL import Image, ImageDraw, ImageFont
-import os, glob, re, config
+import os, glob, re, config, time
 
 def find_latest_img(img_folder="./logs/", img_name="generated_plot_e", img_ext=".png"):
     full_path = img_folder + img_name + "*" + img_ext
@@ -26,9 +26,13 @@ def create_gif(frame_duration_ms=config.FRAME_DURATION, max_epochs=300, image_fo
     
     # Load images
     images = []
+    start_time = time.time()
+    total_time = time.time()
     for epoch in range(1, latest_img_number + 1):
         if (epoch % 50) == 0:
-            print(f"{epoch} epochs")
+            duration = time.time() - start_time
+            start_time = time.time()
+            print(f"{epoch} epochs in {duration:.2f} seconds")
         
         for version in range(config.INTERPOLATION_STEPS):
             filename = f"generated_plot_e{str(epoch)}-{version}.png"
@@ -44,7 +48,8 @@ def create_gif(frame_duration_ms=config.FRAME_DURATION, max_epochs=300, image_fo
     if images:
         # Save the images as a gif
         images[0].save(name, save_all=True, append_images=images[1:], duration=frame_duration_ms, loop=1)
-        print(f"GIF successfully created from images through epoch {latest_img_number} and saved to {output_path}{str(epoch)}")
+        total_time = time.time() - total_time
+        print(f"GIF successfully created from images through epoch {latest_img_number} and saved to {output_path}{str(epoch)} in time {total_time:.2f}")
     else:
         print("No images found to create a gif.")
 # Usage
